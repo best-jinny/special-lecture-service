1d8a7b208d9baa8bf3a1c04725392b682cadcfa3
hyejin <iwilldotherightthings@gmail.com>
2024-12-27 00:19:07 +0900

    Initial commit

---
+++ src/main/java/com/hhplus/lecture/SpecialLectureServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialLectureServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialLectureServiceApplication.class, args);
+	}
+
+}
